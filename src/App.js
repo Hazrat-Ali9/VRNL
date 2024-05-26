ee65b5f7c5f6a34943e44e754c576fdecf1beb17
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-26 19:32:29 +0600

    vrnlexch added :rocket:

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Home from './Components/Home';
+import Nav from './Components/Nav';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
